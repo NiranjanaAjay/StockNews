69fc017fb30a655f9bf567683523015a2645820b
Niranjana Ajay <ajayniranjana05@gmail.com>
2025-04-21 13:49:59 +0530

    change in value calculated

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import os
 import requests
 from dotenv import load_dotenv
 
+load_dotenv()
+
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
+STOCK_API_KEY = os.environ["STOCK_KEY"]
 
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
@@ -26,3 +29,19 @@ Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?.
 Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
 """
 
+parameters ={
+    "function":"TIME_SERIES_DAILY",
+    "symbol":STOCK,
+    "apikey": STOCK_API_KEY
+}
+response = requests.get(url="https://www.alphavantage.co/query",params=parameters)
+response.raise_for_status()
+stock_data = response.json()
+
+daily_stock = stock_data["Time Series (Daily)"]
+dates = list(daily_stock.keys())
+
+yesterday_closing = float(daily_stock[dates[1]]["4. close"])
+day_before_closing = float(daily_stock[dates[2]]["4. close"])
+
+percentage_change = round((abs(yesterday_closing - day_before_closing)/day_before_closing)*100, 1)
